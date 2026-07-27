84549696ac1e180aec8cdd178ed98f1960df7ccd
kaleab16 <classkaleab@gmail.com>
2026-07-27 11:06:55 +0300

    0x0A: Task 3 - is_kind_of_class function using isinstance() for instance or inheritance check

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines a function to check if an object is an instance or inherited."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True if obj is an instance of a_class or inherited from it."""
+    return isinstance(obj, a_class)
