2ee927a0e4dd06f7471e8a4e46ee60d4fd25e825
kaleab16 <classkaleab@gmail.com>
2026-07-27 08:36:53 +0300

    0x06: Task 5 - Square my_print method

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,29 +1,43 @@
 #!/usr/bin/python3
-"""Defines a Square class with printing method."""
+"""Defines a Square class that can print itself."""
 
 
 class Square:
     """Represents a square."""
 
     def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
 
     @property
     def size(self):
+        """Retrieve the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        """Set the size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        return self.__size ** 2
+        """Return the current area of the square."""
+        return self.__size * self.__size
 
     def my_print(self):
+        """Print the square with the '#' character."""
         if self.__size == 0:
             print()
             return
