0490d213fb814ea6108cd8ca455859f45a5246f9
kaleab16 <classkaleab@gmail.com>
2026-08-08 17:44:40 +0300

    Add 0-add_integer.py: add integers with TDD doctest suite

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""0-add_integer module.
+
+Defines a function that adds two integers,
+casting floats to integers first.
+"""
+
+
+def add_integer(a, b=98):
+    """Add two integers or floats, casting floats to int first."""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
