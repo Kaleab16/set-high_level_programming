8f68b14e00d87d37ac8382365926678897801b64
kaleab16 <classkaleab@gmail.com>
2026-08-08 17:15:10 +0300

    Add 10-student.py: Student class with filterable to_json

---
+++ python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with a filterable to_json method."""
+
+
+class Student:
+    """Represents a student with a first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dict representation, optionally filtered by attrs."""
+        if type(attrs) is list and all(type(a) is str for a in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
