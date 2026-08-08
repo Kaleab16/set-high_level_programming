a49c43ec6cd58e6ffb30ad07962da90bee3062a1
kaleab16 <classkaleab@gmail.com>
2026-08-08 17:58:53 +0300

    Add 4-print_square.py: print a square with TDD tests

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""4-print_square module.
+
+Defines a function that prints a square of '#' characters.
+"""
+
+
+def print_square(size):
+    """Print a square of size `size` using the '#' character."""
+    if not isinstance(size, (int, float)) or isinstance(size, bool):
+        raise TypeError("size must be an integer")
+
+    if isinstance(size, float):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
