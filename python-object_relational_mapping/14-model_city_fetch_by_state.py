a9fb7123cc4128aa5d1731f4fe550ed22f0c243a
kaleab16 <classkaleab@gmail.com>
2026-08-23 07:10:03 +0300

    Add model_city.py and 14-model_city_fetch_by_state.py: City ORM model and state-joined listing

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Script that lists all City objects joined with their State.
+
+This module connects to a MySQL server using SQLAlchemy and prints
+every City, joined with its owning State, ordered by `cities.id`
+in ascending order, in the format:
+<state name>: (<city id>) <city name>
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    results = session.query(City, State).join(
+        State, City.state_id == State.id
+    ).order_by(City.id).all()
+
+    for city, state in results:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.close()
