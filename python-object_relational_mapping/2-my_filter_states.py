c160479fc251afa2ff430079a8c046356b1e307b
kaleab16 <classkaleab@gmail.com>
2026-08-28 19:23:56 +0300

    Fix state filtering script

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-"""Script that lists all states matching a user-provided name.
-
-This module connects to a MySQL server using MySQLdb and prints
-every row in the `states` table whose `name` matches the given
-argument, ordered by `id` in ascending order. The query is built
-using string formatting, as required by this exercise.
-"""
 import MySQLdb
 import sys
+
 
 if __name__ == "__main__":
     username = sys.argv[1]
@@ -20,18 +14,19 @@ if __name__ == "__main__":
         port=3306,
         user=username,
         passwd=password,
-        db=database,
-        charset="utf8"
+        db=database
     )
 
-    cursor = db.cursor()
+    cur = db.cursor()
+
     query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
         state_name
     )
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    for row in rows:
+
+    cur.execute(query)
+
+    for row in cur.fetchall():
         print(row)
 
-    cursor.close()
+    cur.close()
     db.close()
